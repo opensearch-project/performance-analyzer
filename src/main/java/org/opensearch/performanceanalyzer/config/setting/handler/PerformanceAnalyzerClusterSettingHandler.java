f5c45d95de83729eea4d944b5f4adbab9184e00c
David Z <38449481+dzane17@users.noreply.github.com>
2023-01-10 12:30:54 -1000

    Cluster config api return verbose PA status (#342)
    * Cluster config api return verbose PA status
    
    Signed-off-by: David Zane <davizane@amazon.com>
    (cherry picked from commit 1f35eebbc9b25d46eb2b81463be7e2ba4e7bfc91)

--- src/main/java/org/opensearch/performanceanalyzer/config/setting/handler/PerformanceAnalyzerClusterSettingHandler.java
+++ src/main/java/org/opensearch/performanceanalyzer/config/setting/handler/PerformanceAnalyzerClusterSettingHandler.java
@@ -6,6 +6,8 @@
 package org.opensearch.performanceanalyzer.config.setting.handler;
 
 
+import java.util.LinkedHashMap;
+import java.util.Map;
 import org.opensearch.performanceanalyzer.config.PerformanceAnalyzerController;
 import org.opensearch.performanceanalyzer.config.setting.ClusterSettingListener;
 import org.opensearch.performanceanalyzer.config.setting.ClusterSettingsManager;
@@ -33,6 +35,13 @@ public class PerformanceAnalyzerClusterSettingHandler implements ClusterSettingL
             PerformanceAnalyzerClusterSettings.PerformanceAnalyzerFeatureBits
                     .THREAD_CONTENTION_MONITORING_BIT
                     .ordinal();
+
+    static final String PA_ENABLED_KEY = "PerformanceAnalyzerEnabled";
+    static final String RCA_ENABLED_KEY = "RcaEnabled";
+    static final String LOGGING_ENABLED_KEY = "LoggingEnabled";
+    static final String BATCH_METRICS_ENABLED_KEY = "BatchMetricsEnabled";
+    static final String THREAD_CONTENTION_MONITORING_ENABLED_KEY =
+            "ThreadContentionMonitoringEnabled";
 
     private final PerformanceAnalyzerController controller;
     private final ClusterSettingsManager clusterSettingsManager;
@@ -134,6 +143,22 @@ public class PerformanceAnalyzerClusterSettingHandler implements ClusterSettingL
      */
     public int getCurrentClusterSettingValue() {
         return currentClusterSetting;
+    }
+
+    public Map<String, Boolean> getCurrentClusterSettingValueVerbose() {
+        Map<String, Boolean> statusMap = new LinkedHashMap<String, Boolean>();
+        statusMap.put(PA_ENABLED_KEY, getPAStateFromSetting(currentClusterSetting.intValue()));
+        statusMap.put(RCA_ENABLED_KEY, getRcaStateFromSetting(currentClusterSetting.intValue()));
+        statusMap.put(
+                LOGGING_ENABLED_KEY, getLoggingStateFromSetting(currentClusterSetting.intValue()));
+        statusMap.put(
+                BATCH_METRICS_ENABLED_KEY,
+                getBatchMetricsStateFromSetting(currentClusterSetting.intValue()));
+        statusMap.put(
+                THREAD_CONTENTION_MONITORING_ENABLED_KEY,
+                getThreadContentionMonitoringStateFromSetting(currentClusterSetting.intValue()));
+
+        return statusMap;
     }
 
     /**
