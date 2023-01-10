1f35eebbc9b25d46eb2b81463be7e2ba4e7bfc91
David Z <38449481+dzane17@users.noreply.github.com>
2023-01-10 12:30:54 -1000

    Cluster config api return verbose PA status (#342)
    * Cluster config api return verbose PA status
    
    Signed-off-by: David Zane <davizane@amazon.com>

--- src/test/java/org/opensearch/performanceanalyzer/config/setting/handler/PerformanceAnalyzerClusterSettingHandlerTests.java
+++ src/test/java/org/opensearch/performanceanalyzer/config/setting/handler/PerformanceAnalyzerClusterSettingHandlerTests.java
@@ -9,6 +9,8 @@ import static org.junit.Assert.assertEquals;
 import static org.mockito.MockitoAnnotations.initMocks;
 import static org.powermock.api.mockito.PowerMockito.when;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
@@ -18,6 +20,16 @@ import org.opensearch.performanceanalyzer.config.setting.ClusterSettingsManager;
 public class PerformanceAnalyzerClusterSettingHandlerTests {
     private static final Boolean DISABLED_STATE = Boolean.FALSE;
     private static final Boolean ENABLED_STATE = Boolean.TRUE;
+
+    private final Map<String, Boolean> ALL_ENABLED_CLUSTER =
+            createStatusMap(
+                    ENABLED_STATE, ENABLED_STATE, ENABLED_STATE, ENABLED_STATE, ENABLED_STATE);
+    private final Map<String, Boolean> ALL_DISABLED_CLUSTER =
+            createStatusMap(
+                    DISABLED_STATE, DISABLED_STATE, DISABLED_STATE, DISABLED_STATE, DISABLED_STATE);
+    private final Map<String, Boolean> MIXED_STATUS_CLUSTER =
+            createStatusMap(
+                    ENABLED_STATE, ENABLED_STATE, DISABLED_STATE, DISABLED_STATE, DISABLED_STATE);
 
     private PerformanceAnalyzerClusterSettingHandler clusterSettingHandler;
 
@@ -37,6 +49,8 @@ public class PerformanceAnalyzerClusterSettingHandlerTests {
                 new PerformanceAnalyzerClusterSettingHandler(
                         mockPerformanceAnalyzerController, mockClusterSettingsManager);
         assertEquals(0, clusterSettingHandler.getCurrentClusterSettingValue());
+        assertEquals(
+                ALL_DISABLED_CLUSTER, clusterSettingHandler.getCurrentClusterSettingValueVerbose());
     }
 
     @Test
@@ -47,6 +61,8 @@ public class PerformanceAnalyzerClusterSettingHandlerTests {
                 new PerformanceAnalyzerClusterSettingHandler(
                         mockPerformanceAnalyzerController, mockClusterSettingsManager);
         assertEquals(31, clusterSettingHandler.getCurrentClusterSettingValue());
+        assertEquals(
+                ALL_ENABLED_CLUSTER, clusterSettingHandler.getCurrentClusterSettingValueVerbose());
     }
 
     @Test
@@ -57,6 +73,8 @@ public class PerformanceAnalyzerClusterSettingHandlerTests {
                 new PerformanceAnalyzerClusterSettingHandler(
                         mockPerformanceAnalyzerController, mockClusterSettingsManager);
         assertEquals(0, clusterSettingHandler.getCurrentClusterSettingValue());
+        assertEquals(
+                ALL_DISABLED_CLUSTER, clusterSettingHandler.getCurrentClusterSettingValueVerbose());
     }
 
     @Test
@@ -67,8 +85,12 @@ public class PerformanceAnalyzerClusterSettingHandlerTests {
                 new PerformanceAnalyzerClusterSettingHandler(
                         mockPerformanceAnalyzerController, mockClusterSettingsManager);
         assertEquals(3, clusterSettingHandler.getCurrentClusterSettingValue());
+        assertEquals(
+                MIXED_STATUS_CLUSTER, clusterSettingHandler.getCurrentClusterSettingValueVerbose());
         clusterSettingHandler.onSettingUpdate(0);
         assertEquals(0, clusterSettingHandler.getCurrentClusterSettingValue());
+        assertEquals(
+                ALL_DISABLED_CLUSTER, clusterSettingHandler.getCurrentClusterSettingValueVerbose());
     }
 
     private void setControllerValues(
@@ -85,5 +107,24 @@ public class PerformanceAnalyzerClusterSettingHandlerTests {
                 .thenReturn(batchMetricsEnabled);
         when(mockPerformanceAnalyzerController.isThreadContentionMonitoringEnabled())
                 .thenReturn(threadContentionMonitoringEnabled);
+    }
+
+    private Map<String, Boolean> createStatusMap(
+            final Boolean paEnabled,
+            final Boolean rcaEnabled,
+            final Boolean loggingEnabled,
+            final Boolean batchMetricsEnabled,
+            final Boolean threadContentionMonitoringEnabled) {
+        Map<String, Boolean> statusMap = new LinkedHashMap<String, Boolean>();
+        statusMap.put(PerformanceAnalyzerClusterSettingHandler.PA_ENABLED_KEY, paEnabled);
+        statusMap.put(PerformanceAnalyzerClusterSettingHandler.RCA_ENABLED_KEY, rcaEnabled);
+        statusMap.put(PerformanceAnalyzerClusterSettingHandler.LOGGING_ENABLED_KEY, loggingEnabled);
+        statusMap.put(
+                PerformanceAnalyzerClusterSettingHandler.BATCH_METRICS_ENABLED_KEY,
+                batchMetricsEnabled);
+        statusMap.put(
+                PerformanceAnalyzerClusterSettingHandler.THREAD_CONTENTION_MONITORING_ENABLED_KEY,
+                threadContentionMonitoringEnabled);
+        return statusMap;
     }
 }
