4b32348bab347ec2d1eb9403c72abbda3d0664e4
Khushboo Rajput <59671881+khushbr@users.noreply.github.com>
2023-10-10 12:55:55 -0700

    Add separate metric for cluster manager service events and metrics (#579)
    * Add separate metric for cluster manager service events and metrics
    
    * Fix build error related to missing import
    
    Signed-off-by: Khushboo Rajput <khushbr@amazon.com>
    
    * Fix test build failure
    
    Signed-off-by: Khushboo Rajput <khushbr@amazon.com>
    
    ---------
    
    Signed-off-by: Khushboo Rajput <khushbr@amazon.com>
    (cherry picked from commit 951df934428562f1c0978310c6e3909c03e4aeda)

--- src/test/java/org/opensearch/performanceanalyzer/collectors/StatsTests.java
+++ src/test/java/org/opensearch/performanceanalyzer/collectors/StatsTests.java
@@ -43,13 +43,13 @@ public class StatsTests {
 
     static Random RANDOM = new Random();
     private static final int MAX_COUNT = 500;
-    private static final int CLUSTER_MANAGER_METRICS_ERRORS =
+    private static final int CLUSTER_MANAGER_SERVICE_EVENTS_METRICS_ERROR =
             Math.abs(RANDOM.nextInt() % MAX_COUNT);
     private static final int REQUEST_REMOTE_ERRORS = Math.abs(RANDOM.nextInt() % MAX_COUNT);
     private static final int READER_PARSER_ERRORS = Math.abs(RANDOM.nextInt() % MAX_COUNT);
     private static final int READER_RESTART_PROCESSINGS = Math.abs(RANDOM.nextInt() % MAX_COUNT);
     private static final int TOTAL_ERRORS =
-            CLUSTER_MANAGER_METRICS_ERRORS
+            CLUSTER_MANAGER_SERVICE_EVENTS_METRICS_ERROR
                     + REQUEST_REMOTE_ERRORS
                     + READER_PARSER_ERRORS
                     + READER_RESTART_PROCESSINGS;
@@ -63,8 +63,9 @@ public class StatsTests {
 
         LinkedList<StatExceptionCode> exceptionCodeList = new LinkedList<>();
 
-        for (int i = 0; i < CLUSTER_MANAGER_METRICS_ERRORS; i++) {
-            exceptionCodeList.add(StatExceptionCode.CLUSTER_MANAGER_METRICS_ERROR);
+        for (int i = 0; i < CLUSTER_MANAGER_SERVICE_EVENTS_METRICS_ERROR; i++) {
+            exceptionCodeList.add(
+                    StatExceptionCode.CLUSTER_MANAGER_SERVICE_EVENTS_METRICS_COLLECTOR_ERROR);
         }
 
         for (int i = 0; i < REQUEST_REMOTE_ERRORS; i++) {
@@ -101,10 +102,12 @@ public class StatsTests {
         assertEquals(
                 sc.getCounters()
                         .getOrDefault(
-                                StatExceptionCode.CLUSTER_MANAGER_METRICS_ERROR.toString(),
+                                StatExceptionCode
+                                        .CLUSTER_MANAGER_SERVICE_EVENTS_METRICS_COLLECTOR_ERROR
+                                        .toString(),
                                 DEFAULT_VAL)
                         .get(),
-                CLUSTER_MANAGER_METRICS_ERRORS);
+                CLUSTER_MANAGER_SERVICE_EVENTS_METRICS_ERROR);
         assertEquals(
                 sc.getCounters()
                         .getOrDefault(
