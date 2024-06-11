17611035b0519b96de809da2ccb07ea9b4107ede
Atharva Sharma <60044988+atharvasharma61@users.noreply.github.com>
2024-06-11 18:18:02 +0530

    changes to merge PA-RTF (#651)
    * PA RTF merging init
    
    Signed-off-by: Atharva Sharma <atharvasharma61@gmail.com>
    
    * working model
    
    Signed-off-by: Atharva Sharma <atharvasharma61@gmail.com>
    
    * working model tip
    
    Signed-off-by: Atharva Sharma <atharvasharma61@gmail.com>
    
    * functional model init
    
    Signed-off-by: Atharva Sharma <atharvasharma61@gmail.com>
    
    * Migrated HeapMetricsCollector
    
    Signed-off-by: Atharva Sharma <atharvasharma61@gmail.com>
    
    * Added RTFThreadPoolMetricsCollector
    
    Signed-off-by: Atharva Sharma <atharvasharma61@gmail.com>
    
    * migrated NodeStats and DiskMetricsCollector
    
    Signed-off-by: Atharva Sharma <atharvasharma61@gmail.com>
    
    * Added gauge data model for Heap_Max metric
    
    Signed-off-by: Atharva Sharma <atharvasharma61@gmail.com>
    
    * implemented TelemetryAwarePlugin
    
    Signed-off-by: Atharva Sharma <atharvasharma61@gmail.com>
    
    * Framework changes for PA RTF merging
    
    Signed-off-by: Atharva Sharma <atharvasharma61@gmail.com>
    
    * refactored
    
    Signed-off-by: Atharva Sharma <atharvasharma61@gmail.com>
    
    * spotless applied
    
    Signed-off-by: Atharva Sharma <atharvasharma61@gmail.com>
    
    * Addressed small comments
    
    Signed-off-by: Atharva Sharma <atharvasharma61@gmail.com>
    
    * Added different flag for RCA collectors
    
    Signed-off-by: Atharva Sharma <atharvasharma61@gmail.com>
    
    * Addressed more comments
    
    Signed-off-by: Atharva Sharma <atharvasharma61@gmail.com>
    
    * Added RTF collectors in config map
    
    Signed-off-by: Atharva Sharma <atharvasharma61@gmail.com>
    
    * Added UTs
    
    Signed-off-by: Atharva Sharma <atharvasharma61@gmail.com>
    
    * Added further UTs
    
    Signed-off-by: Atharva Sharma <atharvasharma61@gmail.com>
    
    * Added dynamic control support to all collectors
    
    Signed-off-by: Atharva Sharma <atharvasharma61@gmail.com>
    
    * fixed UT
    
    Signed-off-by: Atharva Sharma <atharvasharma61@gmail.com>
    
    * refactoring
    
    Signed-off-by: Atharva Sharma <atharvasharma61@gmail.com>
    
    * Revert "refactoring"
    
    This reverts commit 25d66e8bb3554d9d7674a87201beee0b08a2a33d.
    
    Signed-off-by: Atharva Sharma <atharvasharma61@gmail.com>
    
    * Revert "fixed UT"
    
    This reverts commit 369bd957f473c52ae39284f1d7257e254be8d036.
    
    Signed-off-by: Atharva Sharma <atharvasharma61@gmail.com>
    
    * Revert "Added dynamic control support to all collectors"
    
    This reverts commit 447e15f15084ce911b8944446e54d8d36e0e2a6f.
    
    Signed-off-by: Atharva Sharma <atharvasharma61@gmail.com>
    
    * Adding two new collector interfaces
    
    Signed-off-by: Atharva Sharma <atharvasharma61@gmail.com>
    
    * simplified interfaces
    
    Signed-off-by: Atharva Sharma <atharvasharma61@gmail.com>
    
    * Added units and javadocs
    
    Signed-off-by: Atharva Sharma <atharvasharma61@gmail.com>
    
    * Changes metrics semantic conventions
    
    Signed-off-by: Atharva Sharma <atharvasharma61@gmail.com>
    
    * refactored
    
    Signed-off-by: Atharva Sharma <atharvasharma61@gmail.com>
    
    * fixed UT
    
    Signed-off-by: Atharva Sharma <atharvasharma61@gmail.com>
    
    * Added stats metrics for rtf collectors
    
    Signed-off-by: Atharva Sharma <atharvasharma61@gmail.com>
    
    * reverted test delete
    
    Signed-off-by: Atharva Sharma <atharvasharma61@gmail.com>
    
    * Fixes javadoc compilation issue
    
    Signed-off-by: Gagan Juneja <gjjuneja@amazon.com>
    
    ---------
    
    Signed-off-by: Atharva Sharma <atharvasharma61@gmail.com>
    Signed-off-by: Gagan Juneja <gjjuneja@amazon.com>
    Co-authored-by: Gagan Juneja <gjjuneja@amazon.com>

---
+++ src/test/java/org/opensearch/performanceanalyzer/config/setting/handler/PerformanceAnalyzerCollectorsSettingHandlerTests.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.opensearch.performanceanalyzer.config.setting.handler;
+
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+import static org.mockito.MockitoAnnotations.initMocks;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.opensearch.performanceanalyzer.config.PerformanceAnalyzerController;
+import org.opensearch.performanceanalyzer.config.setting.ClusterSettingsManager;
+
+public class PerformanceAnalyzerCollectorsSettingHandlerTests {
+    private PerformanceAnalyzerCollectorsSettingHandler handler;
+    @Mock private PerformanceAnalyzerController controller;
+    @Mock private ClusterSettingsManager clusterSettingsManager;
+
+    @Before
+    public void init() {
+        initMocks(this);
+        handler =
+                new PerformanceAnalyzerCollectorsSettingHandler(controller, clusterSettingsManager);
+    }
+
+    @Test
+    public void testOnSettingUpdate() {
+        Integer newSettingValue = null;
+        handler.onSettingUpdate(newSettingValue);
+        verify(controller, never()).updateCollectorsSetting(anyInt());
+
+        newSettingValue = 1;
+        handler.onSettingUpdate(newSettingValue);
+        verify(controller).updateCollectorsSetting(newSettingValue);
+    }
+}
