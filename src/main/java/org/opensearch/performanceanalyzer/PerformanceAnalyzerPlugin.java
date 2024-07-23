5ace5ac04aca918813dd6a9fe317d618e532f368
Atharva Sharma <atharvasharma61@gmail.com>
2024-07-23 23:34:55 +0530

    removed wildcard imports
    Signed-off-by: Atharva Sharma <atharvasharma61@gmail.com>

--- src/main/java/org/opensearch/performanceanalyzer/PerformanceAnalyzerPlugin.java
+++ src/main/java/org/opensearch/performanceanalyzer/PerformanceAnalyzerPlugin.java
@@ -55,7 +55,12 @@ import org.opensearch.performanceanalyzer.collectors.SearchBackPressureStatsColl
 import org.opensearch.performanceanalyzer.collectors.ShardIndexingPressureMetricsCollector;
 import org.opensearch.performanceanalyzer.collectors.ShardStateCollector;
 import org.opensearch.performanceanalyzer.collectors.ThreadPoolMetricsCollector;
-import org.opensearch.performanceanalyzer.collectors.telemetry.*;
+import org.opensearch.performanceanalyzer.collectors.telemetry.RTFCacheConfigMetricsCollector;
+import org.opensearch.performanceanalyzer.collectors.telemetry.RTFDisksCollector;
+import org.opensearch.performanceanalyzer.collectors.telemetry.RTFHeapMetricsCollector;
+import org.opensearch.performanceanalyzer.collectors.telemetry.RTFNodeStatsAllShardsMetricsCollector;
+import org.opensearch.performanceanalyzer.collectors.telemetry.RTFOSMetricsCollector;
+import org.opensearch.performanceanalyzer.collectors.telemetry.RTFThreadPoolMetricsCollector;
 import org.opensearch.performanceanalyzer.commons.OSMetricsGeneratorFactory;
 import org.opensearch.performanceanalyzer.commons.collectors.DisksCollector;
 import org.opensearch.performanceanalyzer.commons.collectors.GCInfoCollector;
