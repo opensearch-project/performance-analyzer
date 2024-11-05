07f3d6aafa387530386c4ec4287a1a7f7fb09f1f
Gagan Juneja <gagandeepjuneja@gmail.com>
2024-11-05 21:45:05 +0530

    Fixes the re enabling of RTFCacheConfigMetricsCollector (#759)
    Signed-off-by: Gagan Juneja <gjjuneja@amazon.com>
    Co-authored-by: Gagan Juneja <gjjuneja@amazon.com>
    (cherry picked from commit cdd56a07f3eb73b65114ff974c95bdcc163ffca4)

--- src/test/java/org/opensearch/performanceanalyzer/collectors/telemetry/RTFCacheConfigMetricsCollectorTests.java
+++ src/test/java/org/opensearch/performanceanalyzer/collectors/telemetry/RTFCacheConfigMetricsCollectorTests.java
@@ -7,12 +7,15 @@ package org.opensearch.performanceanalyzer.collectors.telemetry;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.atLeastOnce;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
+import java.io.Closeable;
 import java.io.IOException;
 import org.junit.After;
 import org.junit.Before;
@@ -31,6 +34,7 @@ public class RTFCacheConfigMetricsCollectorTests extends OpenSearchSingleNodeTes
     private RTFCacheConfigMetricsCollector rtfCacheConfigMetricsCollector;
     private static MetricsRegistry metricsRegistry;
     private static MetricsRegistry metricsRegistry1;
+    PerformanceAnalyzerController mockController;
     private long startTimeInMills = 1153721339;
 
     @Before
@@ -43,7 +47,7 @@ public class RTFCacheConfigMetricsCollectorTests extends OpenSearchSingleNodeTes
         IndicesService indicesService = getInstanceFromNode(IndicesService.class);
         OpenSearchResources.INSTANCE.setIndicesService(indicesService);
         ConfigOverridesWrapper mockWrapper = mock(ConfigOverridesWrapper.class);
-        PerformanceAnalyzerController mockController = mock(PerformanceAnalyzerController.class);
+        mockController = mock(PerformanceAnalyzerController.class);
         Mockito.when(mockController.isCollectorDisabled(any(), anyString())).thenReturn(false);
         rtfCacheConfigMetricsCollector =
                 spy(new RTFCacheConfigMetricsCollector(mockController, mockWrapper));
@@ -72,6 +76,41 @@ public class RTFCacheConfigMetricsCollectorTests extends OpenSearchSingleNodeTes
         OpenSearchResources.INSTANCE.setMetricsRegistry(metricsRegistry1);
         rtfCacheConfigMetricsCollector.collectMetrics(startTimeInMills);
         verify(metricsRegistry1, never())
+                .createGauge(anyString(), anyString(), anyString(), any(), any());
+    }
+
+    @Test
+    public void testCollectMetricsDisableEnable() throws IOException {
+        createIndex(TEST_INDEX);
+        Closeable fieldCacheGaugeObservable = Mockito.mock(Closeable.class);
+        Closeable requestCacheGaugeObservable = Mockito.mock(Closeable.class);
+
+        Mockito.when(
+                        metricsRegistry.createGauge(
+                                anyString(), anyString(), anyString(), any(), any()))
+                .thenReturn(requestCacheGaugeObservable, fieldCacheGaugeObservable);
+        rtfCacheConfigMetricsCollector.collectMetrics(startTimeInMills);
+        verify(metricsRegistry, times(2))
+                .createGauge(anyString(), anyString(), anyString(), any(), any());
+
+        OpenSearchResources.INSTANCE.setMetricsRegistry(metricsRegistry1);
+        Mockito.when(
+                        mockController.isCollectorDisabled(
+                                any(), eq("RTFCacheConfigMetricsCollector")))
+                .thenReturn(true);
+        rtfCacheConfigMetricsCollector.collectMetrics(startTimeInMills);
+        verify(fieldCacheGaugeObservable, Mockito.only()).close();
+        verify(requestCacheGaugeObservable, Mockito.only()).close();
+        verify(metricsRegistry1, never())
+                .createGauge(anyString(), anyString(), anyString(), any(), any());
+
+        OpenSearchResources.INSTANCE.setMetricsRegistry(metricsRegistry1);
+        Mockito.when(
+                        mockController.isCollectorDisabled(
+                                any(), eq("RTFCacheConfigMetricsCollector")))
+                .thenReturn(false);
+        rtfCacheConfigMetricsCollector.collectMetrics(startTimeInMills);
+        verify(metricsRegistry1, times(2))
                 .createGauge(anyString(), anyString(), anyString(), any(), any());
     }
 }
